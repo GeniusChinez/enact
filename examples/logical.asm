803a53d8c74dd974cdbb86396b15cf0847776ffa
geniuschinez <geniuschinembiri@gmail.com>
2021-02-23 05:28:34 +0200

    Added example code for logical operations

---
+++ examples/logical.asm
@@ -0,0 +1,28 @@
+# logical AND
+    iconst 7
+    iconst 3
+    and
+    dump
+    pop
+
+# logical OR 
+    iconst 4
+    iconst 1
+    or
+    dump
+    pop
+
+# logical NOT 
+    iconst 8
+    not
+    dump
+    pop
+
+# logical XOR
+    iconst 4
+    iconst 8
+    xor
+    dump
+    pop
+
+halt
