e9491c2299953cf794e98b61278b0d736b78718d
geniuschinez <geniuschinembiri@gmail.com>
2021-02-23 05:00:53 +0200

    Added example code for arithmetic operations

---
+++ examples/arithmetic.asm
@@ -0,0 +1,49 @@
+iconst 3
+iconst 4
+add
+dump
+pop
+
+iconst 4
+iconst 3
+sub
+dump
+pop
+
+iconst 3
+iconst 4
+mul
+dump
+pop
+
+iconst 8
+iconst 4
+div
+dump
+pop
+
+iconst 8
+iconst 4
+mod
+dump
+pop
+
+iconst 2
+iconst 3
+pow
+dump
+pop
+
+iconst 4
+iconst 2
+shl
+dump
+pop
+
+iconst 4
+iconst 2
+shr
+dump
+pop
+
+halt
